57bb06bf9673333395b77410d358b36598c0aa5b
Vanderson Andrade <vandersonandr@gmail.com>
2019-07-13 23:21:37 -0300

    Primeiro Model-Controller-View --> Departamentos

---
+++ SalesWebMvc/Models/Departamento.cs
@@ -0,0 +1,9 @@
+﻿namespace SalesWebMvc.Models
+{
+    public class Departamento
+    {
+        //Clase Basica do departamento.
+        public int Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
